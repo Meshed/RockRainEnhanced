4d32be683e0822f9896347572d028bb1de2643f1
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2012-12-26 17:17:40 -0500

    resharper changes

--- RockRainEnhanced/RockRainEnhanced/GameScenes/StartScene.cs
+++ RockRainEnhanced/RockRainEnhanced/GameScenes/StartScene.cs
@@ -27,20 +27,23 @@ namespace RockRainEnhanced.GameScenes
             Help,
             Scores,
             Quit
-
         }
-        TextMenuComponent menu;
-        readonly Texture2D elements;
-        AudioLibrary audio;
-        SpriteBatch spriteBatch = null;
-        Rectangle rockRect = new Rectangle(0, 0, 536, 131);
-        Vector2 rockPosition;
-        Rectangle rainRect = new Rectangle(120, 165, 517, 130);
-        Vector2 rainPosition;
-        Rectangle enhancedRect = new Rectangle(8, 304, 375, 144);
-        Vector2 enhancedPosition;
-        bool showEnchanced;
-        TimeSpan elapsedTime = TimeSpan.Zero;
+
+        private readonly TextMenuComponent _menu;
+
+        private readonly Texture2D elements;
+
+        readonly AudioLibrary _audio;
+
+        readonly SpriteBatch _spriteBatch;
+        Rectangle _rockRect = new Rectangle(0, 0, 536, 131);
+        Vector2 _rockPosition;
+        Rectangle _rainRect = new Rectangle(120, 165, 517, 130);
+        Vector2 _rainPosition;
+        Rectangle _enhancedRect = new Rectangle(8, 304, 375, 144);
+        Vector2 _enhancedPosition;
+        bool _showEnchanced;
+        TimeSpan _elapsedTime = TimeSpan.Zero;
 
         public StartScene(Game game, SpriteFont smallFont, SpriteFont largeFont, Texture2D background, Texture2D elements)
             : base(game)
@@ -49,12 +52,12 @@ namespace RockRainEnhanced.GameScenes
             Components.Add(new ImageComponent(game, background, ImageComponent.DrawMode.Center));
 
             string[] items = Enum.GetValues(typeof(MenuItems)).Cast<MenuItems>().Select(s => s.GetDisplayOrName()).ToArray();
-            menu = new TextMenuComponent(game, smallFont, largeFont);
-            menu.SetMenuItems(items);
-            Components.Add(menu);
+            this._menu = new TextMenuComponent(game, smallFont, largeFont);
+            this._menu.SetMenuItems(items);
+            Components.Add(this._menu);
 
-            spriteBatch = (SpriteBatch)Game.Services.GetService(typeof(SpriteBatch));
-            audio = (AudioLibrary)Game.Services.GetService(typeof(AudioLibrary));
+            this._spriteBatch = (SpriteBatch)Game.Services.GetService(typeof(SpriteBatch));
+            this._audio = (AudioLibrary)Game.Services.GetService(typeof(AudioLibrary));
         }
 
         /// <summary>
@@ -74,38 +77,38 @@ namespace RockRainEnhanced.GameScenes
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         public override void Update(GameTime gameTime)
         {
-            if (!menu.Visible)
+            if (!this._menu.Visible)
             {
-                if (rainPosition.X >= (Game.Window.ClientBounds.Width - 595) / 2)
+                if (this._rainPosition.X >= (Game.Window.ClientBounds.Width - 595) / 2)
                 {
-                    rainPosition.X -= 15;
+                    this._rainPosition.X -= 15;
                 }
-                if (rockPosition.X <= (Game.Window.ClientBounds.Width - 715) / 2)
+                if (this._rockPosition.X <= (Game.Window.ClientBounds.Width - 715) / 2)
                 {
-                    rockPosition.X += 15;
+                    this._rockPosition.X += 15;
                 }
                 else
                 {
-                    menu.Visible = true;
-                    menu.Enabled = true;
+                    this._menu.Visible = true;
+                    this._menu.Enabled = true;
 
-                    MediaPlayer.Play(audio.StartMusic);
+                    MediaPlayer.Play(this._audio.StartMusic);
 
 #if XBOX360
                     enhancedPosition = new Vector2((rainPosition.X + rainRect.Width - enhancedRect.Width/2), rainPosition.Y);
 #else
-                    enhancedPosition = new Vector2((rainPosition.X + rainRect.Width - enhancedRect.Width / 2) - 80, rainPosition.Y);
+                    this._enhancedPosition = new Vector2((this._rainPosition.X + this._rainRect.Width - this._enhancedRect.Width / 2) - 80, this._rainPosition.Y);
 #endif
                 }
             }
             else
             {
-                elapsedTime += gameTime.ElapsedGameTime;
+                this._elapsedTime += gameTime.ElapsedGameTime;
 
-                if (elapsedTime > TimeSpan.FromSeconds(1))
+                if (this._elapsedTime > TimeSpan.FromSeconds(1))
                 {
-                    elapsedTime -= TimeSpan.FromSeconds(1);
-                    showEnchanced = !showEnchanced;
+                    this._elapsedTime -= TimeSpan.FromSeconds(1);
+                    this._showEnchanced = !this._showEnchanced;
                 }
             }
 
@@ -116,29 +119,29 @@ namespace RockRainEnhanced.GameScenes
         {
             base.Draw(gameTime);
 
-            spriteBatch.Draw(elements, rockPosition, rockRect, Color.White);
-            spriteBatch.Draw(elements, rainPosition, rainRect, Color.White);
+            this._spriteBatch.Draw(elements, this._rockPosition, this._rockRect, Color.White);
+            this._spriteBatch.Draw(elements, this._rainPosition, this._rainRect, Color.White);
 
-            if (showEnchanced)
+            if (this._showEnchanced)
             {
-                spriteBatch.Draw(elements, enhancedPosition, enhancedRect, Color.White);
+                this._spriteBatch.Draw(elements, this._enhancedPosition, this._enhancedRect, Color.White);
             }
         }
 
         public override void Show()
         {
-            audio.NewMeteor.Play();
-            rockPosition.X = -1 * rockRect.Width;
-            rockPosition.Y = 40;
-            rainPosition.X = Game.Window.ClientBounds.Width;
-            rainPosition.Y = 180;
+            this._audio.NewMeteor.Play();
+            this._rockPosition.X = -1 * this._rockRect.Width;
+            this._rockPosition.Y = 40;
+            this._rainPosition.X = Game.Window.ClientBounds.Width;
+            this._rainPosition.Y = 180;
 
             // Put the menu centered in screen
-            menu.Position = new Vector2((Game.Window.ClientBounds.Width - menu.Width)/2, 330);
+            this._menu.Position = new Vector2((Game.Window.ClientBounds.Width - this._menu.Width)/2, 330);
 
-            menu.Visible = false;
-            menu.Enabled = false;
-            showEnchanced = false;
+            this._menu.Visible = false;
+            this._menu.Enabled = false;
+            this._showEnchanced = false;
 
             base.Show();
         }
@@ -151,7 +154,7 @@ namespace RockRainEnhanced.GameScenes
 
         public int SelectedMenuIndex
         {
-            get { return menu.SelectedIndex; }
+            get { return this._menu.SelectedIndex; }
         }
     }
 }
